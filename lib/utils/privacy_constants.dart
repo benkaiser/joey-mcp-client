d641a3e1b8f7d13832e8fd33a00b2cabe2de7a24
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2026-02-20 22:13:00 +1000

    Fix privacy policy URL to correct repository

--- lib/utils/privacy_constants.dart
+++ lib/utils/privacy_constants.dart
@@ -4,7 +4,7 @@ class PrivacyConstants {
 
   /// URL to the privacy policy hosted on GitHub.
   static const String privacyPolicyUrl =
-      'https://github.com/benkaiser/joey-mcp-client-flutter/blob/master/docs/PRIVACY_POLICY.md';
+      'https://github.com/benkaiser/joey-mcp-client/blob/master/docs/PRIVACY_POLICY.md';
 
   /// SharedPreferences key for OpenRouter data sharing consent.
   static const String privacyConsentKey = 'privacy_consent_given';
